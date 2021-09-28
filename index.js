4750e969c03ef1651a8893a47e5362c39b1a89cc
Edgar Ortega <edort93@gmail.com>
2021-09-27 21:57:04 -0500

    updating comments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown.js");
-// TODO: Create an array of questions for user input
+// An array of questions for user input
 const questions = [
   {
     type: "input",
@@ -82,7 +82,7 @@ function writeToFile(fileName, data) {
 
 }
 
-// TODO: Create a function to initialize app
+// A function to initialize app
 function init() {
   inquirer.prompt(questions).then((answers) => {
     // console.log(answers.name);
